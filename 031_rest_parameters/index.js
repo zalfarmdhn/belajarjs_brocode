9297ed013bc32e72247cdf418d66f70fc59e5651
zalfarmdhn <zalfarmdhn1011@gmail.com>
2022-05-12 14:14:01 +0700

    belajar 030-032

---
+++ 031_rest_parameters/index.js
@@ -0,0 +1,26 @@
+// rest parameters = represents an indefinite number
+//...                of parameters
+//                   (packs arguments into an array)
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+console.log(sum(a, b, c, d, e));
+
+function sum(a, b) {
+    return a + b;
+}
+
+function sum(...numbers) {
+    let total = 0;
+    for (let number of numbers) {
+        total += number
+    }
+    return total
+}
+
+
+
